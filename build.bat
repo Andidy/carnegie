6bb1b81607cfd73beed2c566c8fbf846d87d73c0
Andidy <37605997+Andidy@users.noreply.github.com>
2020-12-15 02:31:28 -0600

    commented out release build in build.bat

--- build.bat
+++ build.bat
@@ -14,16 +14,16 @@ cl -WX -W4 -wd4100 -wd4201 ^
  ..\libs\pix\WinPixEventRuntime.lib ^
  -nologo
 
-cl -WX -W4 -wd4100 -wd4201 ^
- -wd4238 ^
- -Oi -GR -EHa- -EHsc ^
- -O2 -MT -Gm- ^
- -Fecarnegie.exe ^
- ..\win32_carnegie.cpp ^
- user32.lib ole32.lib gdi32.lib ^
- Xinput.lib d3d12.lib dxgi.lib ^
- dxguid.lib d3dcompiler.lib ^
- ..\libs\pix\WinPixEventRuntime.lib ^
- -nologo
+REM cl -WX -W4 -wd4100 -wd4201 ^
+REM  -wd4238 ^
+REM  -Oi -GR -EHa- -EHsc ^
+REM  -O2 -MT -Gm- ^
+REM  -Fecarnegie.exe ^
+REM  ..\win32_carnegie.cpp ^
+REM  user32.lib ole32.lib gdi32.lib ^
+REM  Xinput.lib d3d12.lib dxgi.lib ^
+REM  dxguid.lib d3dcompiler.lib ^
+REM  ..\libs\pix\WinPixEventRuntime.lib ^
+REM  -nologo
 
 popd
